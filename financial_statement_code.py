a8420feacc228035be0790deaa950ff33e8eebfd
Benjamin Nguyen <bmn5439@psu.edu>
2026-05-28 16:44:30 -0400

    Made the Index Calculation Function, but with a limit to only certain categories listed in the project

--- financial_statement_code.py
+++ financial_statement_code.py
@@ -7,15 +7,19 @@ Short Description: This program will calculate the index statement from your ban
 import csv
 
 def description():
-    print('Your bank statement must be in a csv file')
+    print('Your bank statement must be in a csv file and have the starting balance on the first row')
+    print('Bank statement only allows Balance, Housing, Food, Income, Utilities, Entertainment, Transport, and Healthcare in categories')
     print('Based of user input of the file location that lists the bank statement, this program will calculate the index.')
+    print('Housing, Utilities and Online Subscriptions is only accounted into the index score')
+    print('Example Fake Bank Statement I used is in the github commited to the main branch')
 
 # gives the user a menu to choose a selection, in which returns the selection to main
 def menu():
     print('Type 1 to enter the file name of the bank statement and read its contents')
     print('Type 2 to display the contents of the banks statement')
-    print('Type 2 to calculate the index of the file name')
-    print('Type 3 to calculate the cost of the one of the statements in a time frame')
+    print('Type 3 to calculate the index of the file name and display it')
+    print('Type 4 to end the program')
+    print()
     choice = int(input('Please enter your choice: '))
     return choice
 
@@ -37,12 +41,12 @@ def read_file():
             print('Please enter another csv file name again')
 
 #Displays the bank statement of the csv file the user inputted earlier
-def bank_display(numbers):
+def bank_display(list2d):
     #Iterates through the strings in the 2d list, and makes sure that and finds the max character of spaces to
     #separate the bank statement
 
     length = 0
-    for row in numbers:
+    for row in list2d:
         for item in row:
             if len(item) > length:
                 length = len(item)
@@ -54,11 +58,44 @@ def bank_display(numbers):
     #Iterates through the strings in the 2d list and adds them through row_string with a format of the max amount of spaces
     #calculated earlier to separate the strings from overlapping to the next column.
 
-    for row in numbers:
+    #The outer loop "for row in numbers" resets the string to be empty each time a new row starts
+    #print(raw_string) must be put underneath for row in numbers in order for the row_string to reset the string each time
+    #or else the string will continue to be printed again each time
+
+    for row in list2d:
         row_string = ''
         for item in row:
             row_string += f'{item:<{column_width}}'
         print(row_string)
+
+def calculate_index(contents):
+    income_row = []
+    debt_row = []
+
+    #detects if the category is an income and adds it to the income_row list to only allow income rows
+    for row in contents:
+        if 'Income' in row or 'Deposit' in row:
+            income_row.append(row)
+        #detects if the category is a debt and adds it to debt_row to allow only debt rows
+        elif 'Housing' in row or 'Utilities' in row or 'Online Subscriptions' in row:
+            debt_row.append(row)
+
+    #only adds the income to the net income
+    net_income = 0
+    for positive_money in income_row:
+        net_income += float(positive_money[5])
+
+    #Estimates the gross income based of the net income using the formula of the net_income / (1-0.25)
+    gross_income = net_income / (1-0.25)
+
+    #Adds all the debt from the debt_row into debt
+    debt = 0
+    for negative_money in debt_row:
+        debt += float(negative_money[4])
+
+    #calculates the index score based of the formula of the (debt/ gross_income) * 100 and returns the score
+    index_score = (debt / gross_income) * 100
+    return f'{index_score:.2f}'
 
 
 # Where all the functions are called
@@ -69,16 +106,28 @@ if __name__ == "__main__":
     flag = 0
 
     #determines when to selection is over and when the other functions are called
-    while selection != 5:
+    while selection != 4:
         if selection == 1:
             raw_statement = read_file()
             flag = 1
-        if selection == 2:
+        elif selection == 2:
             if flag == 0:
                 print('Must choose choice 1 first')
             else:
+                print()
                 bank_display(raw_statement)
+                print()
+        elif selection == 3:
+            if flag == 0:
+                print('Must choose choice 1 first')
+            else:
+                score = calculate_index(raw_statement)
+                print()
+                print(f'Your index score is: {score}')
+                print()
 
         #Goes back to the menu after each function is done
         selection = menu()
+    print()
+    print('Thank you for using this program')
 
